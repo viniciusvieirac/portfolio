7ff940359d675a8c5d103b9a6236328143588380
viniciusvieirac <vini.clau0@gmail.com>
2023-08-03 13:08:10 -0300

    feat: init header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {Fira_Sans} from '@next/font/google';
+
+const firaSans = Fira_Sans({
+  subsets: ['latin'],
+  weight: '400',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={firaSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
+    <header className="flex justify-between items-center max-w-4xl mx-auto mt-8">
+      <h1 className="font-bold">Vieira</h1>
+      <div className="flex space-x-8 font-normal">
+        <a href="">Skills</a>
+        <a href="">Projetos</a>
+        <a href="">Contato</a>
+      </div>
+    </header>
+  );
 }
-
-
-//teste
