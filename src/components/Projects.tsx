a87b9f8302c1c848414d07d4e7c990c772a37d6f
viniciusvieirac <vini.clau0@gmail.com>
2023-08-18 09:47:04 -0300

    fix: bug carousel

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,13 +1,13 @@
 import { randomImages } from "@/utils/projetos";
-import Carousel from "@/components/Carousel";
+import Carousel from "./Carousel";
 
 export default function Projects() {
-    return (
-        <>
-        <section id="projects">
+  return (
+    <>
+      <section id="projects">
         <h1 className="text-center font-bold text-4xl mt-8">Projects</h1>
         <Carousel images={randomImages} />
       </section>
-        </>
-    )
-}
+    </>
+  )
+};
